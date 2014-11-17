dbf8be17956aa8f4d7b365acfd17b45f0396cf09
vagrant <vagrant@lighthouse-labs>
2014-11-17 22:16:37 +0000

    formfix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
   def new
+    
   end
 
   def create
     user = User.find_by(email: params[:email])
-
+    
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to locations_path, notice: "Hello, #{user.name}!"
