93968c25a7f2bdac9108c2333bee1646997a28d1
AndrewGardhouse <AndrewGardhouse@gmail.com>
2014-11-20 18:48:26 +0000

    Users have random jpgs generated for their default avatars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
   def gravatar(email)
     email_address = email.downcase
     hash = Digest::MD5.hexdigest(email_address)
-    default = "http%3A%2F%2Frs242.pbsrc.com%2Falbums%2Fff147%2Fgripofabear1%2Ffunnycat.jpg~c200"
+    default = "http%3A%2F%2Florempixel.com%2F200%2F200%2Fcats%2F"
     image = "http://www.gravatar.com/avatar/#{hash}?s=150&d=#{default}"
     return image
   end
