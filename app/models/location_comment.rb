b85e9c841b723466c44be99afb08583aca7efa1f
vagrant <vagrant@lighthouse-labs>
2014-11-20 20:29:46 +0000

    logicsearch

--- app/models/location_comment.rb
+++ app/models/location_comment.rb
@@ -2,6 +2,6 @@ class LocationComment < ActiveRecord::Base
   belongs_to :location
   belongs_to :user
 
-  validates :vote,
-  numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10}
+  validates :vote, 
+  	numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10}
 end
