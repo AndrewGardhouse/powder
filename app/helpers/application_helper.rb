1eb6d2c90bfd316c3c3270fa1c9a409dfbc0f31d
Kristine Chalk <kristine.chalk@gmail.com>
2014-11-20 15:26:42 -0800

    resizing!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,16 @@
 require 'digest/md5'
 
 module ApplicationHelper
-  def gravatar(email)
+  # Here I get email and size (if nil, defaults to 150)
+  def gravatar(email, size = 150)
     email_address = email.downcase
     hash = Digest::MD5.hexdigest(email_address)
-    default = "http%3A%2F%2Florempixel.com%2F200%2F200%2Fcats%2F"
-    image = "http://www.gravatar.com/avatar/#{hash}?s=150&d=#{default}"
+    # If gravatar does not exists, it laods a kitty image, so use the size here as well! :)
+    default = "http%3A%2F%2Florempixel.com%2F#{size}%2F#{size}%2Fcats%2F"
+    # This string gets the gravatar image, I am using the size here
+    image = "http://www.gravatar.com/avatar/#{hash}?s=#{size}&d=#{default}"
     return image
   end
 end
+
+
