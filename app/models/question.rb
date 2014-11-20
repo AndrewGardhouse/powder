19d4f2075603b7cc1abf66d9efc25d3bfa9c8753
Kristine Chalk <kristine.chalk@gmail.com>
2014-11-20 12:29:17 -0800

    questions main page

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,8 @@
 class Question < ActiveRecord::Base
   has_many   :answers, dependent: :destroy
   belongs_to :user
+
+  def vote_average
+    (answers.sum(:votes).round(4))/answers.size
+  end
 end
